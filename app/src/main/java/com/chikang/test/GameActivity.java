b593f7f968a0019720b74cfce98960a15121d315
Chikang Eng <ceng1@paragoniu.edu.kh>
2020-07-04 16:28:06 +0700

    Game is working like it's suppose to. Adding more features

--- app/src/main/java/com/chikang/test/GameActivity.java
+++ app/src/main/java/com/chikang/test/GameActivity.java
@@ -2,6 +2,7 @@ package com.chikang.test;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,9 +16,13 @@ import java.util.Random;
 public class GameActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button[][] buttons = new Button[4][4];
+    private long time[][] = new long[4][4];
 
     private int Score;
     private int Life = 3;
+
+    private int SpawnTime = 2000; //in milliseconds
+    private int AppearDuration = 5000; //in milliseconds
 
     private TextView txtViewScore;
     private TextView txtViewLife;
@@ -39,7 +44,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
                 buttons[i][j].setOnClickListener(this);
             }
         }
-        Random();
+        handler.postDelayed(runnable, 1000);
 
         Button btn_Reset = findViewById(R.id.button_reset);
         btn_Reset.setOnClickListener(new View.OnClickListener() {
@@ -63,33 +68,77 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         if (((Button) v).getText().toString().equals(goblin)) {
+            ((Button) v).setText("");
             Score++;
 
         }
 
         if (((Button) v).getText().toString().equals(angel)) {
+            ((Button) v).setText("");
             Score -= 10;
             Life--;
         }
         UpdateStat();
-        Random();
     }
 
     public void UpdateStat(){
         txtViewScore.setText("Score: " + Score);
+        if (Score % 10 == 0) {
+            if (SpawnTime > 500) {
+                SpawnTime -= 30;
+            }
+            if (AppearDuration > 500) {
+                AppearDuration -= 100;
+            }
+        }
         txtViewLife.setText("Life: " + Life);
     }
 
     public void Random() {
         Random rand = new Random();
-        int pop = rand.nextInt(10);
-        int i = rand.nextInt(4);
-        int j = rand.nextInt(4);
-        if (pop >= 3) {
-            buttons[i][j].setText(goblin); //Goblin
+        int gobNum = rand.nextInt(6) + 1;
+        int angNum = rand.nextInt(10);
+        int i,j;
+        for (int k = 0; k < gobNum; k++) {
+            i = rand.nextInt(4);
+            j = rand.nextInt(4);
+            buttons[i][j].setText(goblin);
+            time[i][j] = System.currentTimeMillis();
         }
-        else {
-            buttons[i][j].setText(angel);
+        if (angNum > 7) {
+            i = rand.nextInt(4);
+            j = rand.nextInt(4);
+            if (buttons[i][j].getText().toString().equals("")) {
+                buttons[i][j].setText(angel);
+                time[i][j] = System.currentTimeMillis();
+            }
         }
     }
+
+    private Handler handler = new Handler();
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            Random();
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
+                    if (!(buttons[i][j].getText().toString().equals(""))) {
+                        if ((time[i][j] + AppearDuration) < System.currentTimeMillis()) {
+                            if (buttons[i][j].getText().toString().equals(goblin)) {
+                                Score -= 10;
+                                Life--;
+                            } else {
+                                Score++;
+                            }
+                            buttons[i][j].setText("");
+                            UpdateStat();
+                        }
+                    }
+                }
+            }
+
+            handler.postDelayed(this, SpawnTime);
+        }
+    };
+
 }
